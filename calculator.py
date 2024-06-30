35c0e7f3e81f47b871cbfaf30af26eff7d38aa63
Aden <asvay@ucsc.edu>
2024-06-30 21:31:38 +0000

    forgot semicolon

--- calculator.py
+++ calculator.py
@@ -29,7 +29,7 @@ def main():
         choice = input("Enter choice(1/2/3/4): ")
 
         #checking if choice is valid
-        if choice in ['1', '2', '3', '4']
+        if choice in ['1', '2', '3', '4']:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
 
