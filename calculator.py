465b1e3d0179b20d059de44957e778b5c4734860
Aden <asvay@ucsc.edu>
2024-06-30 21:29:28 +0000

    fixing indentation and removing semicolons

--- calculator.py
+++ calculator.py
@@ -19,11 +19,11 @@ def divide(x, y):
 
 def main(): 
     
-    print("Select operation:");
-    print("1. Add");
-    print("2. Subtract");
-    print("3. Multiply");
-    print("4. Divide");
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
 
 
     while True: 
@@ -32,27 +32,26 @@ def main():
 
         #checking if choice is valid
         if choice in ['1', '2', '3', '4']
-            num1 = float(input("Enter first number: "));
-            num2 = float(input("Enter second number: "));
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
 
 
-        if choice == '1':
-            print("Result", add(num1, num2));
-        elif choice == '2': 
-            print("Result", subtract(num1, num2));
-        elif choice == '3':
-            print("Result", multiply(num1, num2));
-        elif choice == '4':
-            print("Result", divide(num1, num2));
-
-    else: 
-        print("Invalid input") 
+            if choice == '1':
+                print("Result", add(num1, num2))
+            elif choice == '2': 
+                print("Result", subtract(num1, num2))
+            elif choice == '3':
+                print("Result", multiply(num1, num2))
+            elif choice == '4':
+                print("Result", divide(num1, num2))
+        else: 
+            print("Invalid input") 
 
 
     # Ask if the user wants to perform another calculation
     next_calculation = input("Would you like to perform another calculation? (yes/no): ")
     if next_calculation.lower() != 'yes':
-        break;
+        break
 
 
 
