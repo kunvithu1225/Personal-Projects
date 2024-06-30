fc2cd76658bc13f3613e35cdef7e49a09edf03f7
Aden <asvay@ucsc.edu>
2024-06-30 21:26:22 +0000

    removed semicolons because python does not need them after definitions

--- calculator.py
+++ calculator.py
@@ -1,18 +1,18 @@
 #Calculator program 
 
 def add(x, y): 
-    return x + y;
+    return x + y
 
 def subtract(x, y):
-    return x - y;
+    return x - y
 
 def multiply(x, y):
-    return x * y;
+    return x * y
 
 def divide(x, y):
     if y == 0: 
         return "Error: Divided by zero" 
-    return x / y;
+    return x / y
 
 
 #List of operations 
@@ -37,13 +37,13 @@ def main():
 
 
         if choice == '1':
-            print(add(num1, num2));
+            print("Result", add(num1, num2));
         elif choice == '2': 
-            print(subtract(num1, num2));
+            print("Result", subtract(num1, num2));
         elif choice == '3':
-            print(multiply(num1, num2));
+            print("Result", multiply(num1, num2));
         elif choice == '4':
-            print(divide(num1, num2));
+            print("Result", divide(num1, num2));
 
     else: 
         print("Invalid input") 
