fa295c8334d5462060bf1152c53b7625890aea55
Aden <asvay@ucsc.edu>
2024-07-02 18:26:16 +0000

    starting vhs translation project

--- khmer_to_english.py
+++ khmer_to_english.py
@@ -1,1 +1,1 @@
-#This is a project that aims to translate dad's khmer vhs into english so that I can watch and understand
+#This is a project that aims to translate dad's khmer vhs into english so that I can watch and understand. 
