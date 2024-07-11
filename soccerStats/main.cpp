530f0aa86b0d67c120a730d3fde8f7d4ee7c3cbe
Aden <asvay@ucsc.edu>
2024-07-11 23:59:21 +0000

    started main.cpp

---
+++ soccerStats/main.cpp
@@ -0,0 +1,25 @@
+// This is the main file that synthesizes all of the files into a primary display
+
+
+#include <iostream>
+#include "team.h"
+
+int main() {
+
+	return 0; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
