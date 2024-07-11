47b72e3562cc9e656c06b195dadf9516fe0adf76
Aden <asvay@ucsc.edu>
2024-07-11 23:48:17 +0000

    transfered player.h into another directory

---
+++ soccerStats/player.h
@@ -0,0 +1,27 @@
+// This is a header file that stores information on the player class
+// We will include information like an individual players stats here
+
+
+#include <iostream> 
+#include <string>
+
+
+class Player {
+public: 
+	std::string name; 
+	std::string position;
+	int goalsScored;
+	int assists;
+	int yellowCards
+	int redCards;
+
+
+	Player(const std::string& playerName, const std::string& playerPosition);
+
+	void displayingInfo();
+	
+	void updateStats(int goals, int assists, int yellow, int red); 
+	};
+
+#endif // PLAYER_H
+
