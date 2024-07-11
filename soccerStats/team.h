f2dd260991eea05a905ef40a0a411497cda70423
Aden <asvay@ucsc.edu>
2024-07-11 23:54:39 +0000

    started the team.h file

---
+++ soccerStats/team.h
@@ -0,0 +1,26 @@
+// This is a file for the stats of a team. In this file we are using information from player.h 
+
+
+
+#include <iostream>
+#include <vector>
+#include "player.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
