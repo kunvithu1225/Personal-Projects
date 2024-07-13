c497c0b4ceade6aa482b045f47414308d8bc662c
Aden <asvay@ucsc.edu>
2024-07-13 21:00:42 +0000

    added new update for summer budgeting

--- budget_calc2024.c
+++ budget_calc2024.c
@@ -1,6 +1,9 @@
 //This is a budget calculator used to manage finances 
+//Update made on July 13, 2024
+
 
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
@@ -12,19 +15,54 @@ int main() {
 	//prompt and user input
 	printf("Enter paycheck amount: $");
 	scanf("%f", &paycheck);
+	
+	// Confirming user input
+	printf("You entered: paycheck");
+	
+	printf("Is this a summer paycheck? (yes/no)");
+	scanf(%3s, response);
 
-	//logic
-	needs = 0.5 * paycheck;
-	wants = 0.3 * paycheck;
-	savings = 0.2 * paycheck;
+	//lower-case conversion
+	for (int i = 0; response[i]; i++) {
+		response[i] = tolower(response[i]);
+	}
 
-	//reads user input
-	printf("\nBudget Breakdown:\n");
-    	printf("Needs: $%.2f\n", needs);
-    	printf("Wants: $%.2f\n", wants);
-    	printf("Savings: $%.2f\n", savings);
+	if (strcmp(reposnse, "yes") == 0) {
+		printf("Here is the summer budget breakdown: ");
 
-		
+		//logic
+                savings = 0.7 * paycheck;
+                checking = 0.2 * paycheck;
+                investment = 0.1 * paycheck;
+
+                //reads user input
+                printf("\nBudget Breakdown:\n");
+                printf("Savings: $%.2f\n", savings);
+                printf("Checking: $%.2f\n", checking);
+                printf("Investment: $%.2f\n", investment);	
+
+
+
+
+	} else if (strcmp(response, "no") == 0) {
+		printf("Here is the standard budget breakdown: ");
+
+		//logic
+		savings = 0.5 * paycheck;
+		checking = 0.3 * paycheck;
+		investment = 0.2 * paycheck;
+
+		//reads user input
+		printf("\nBudget Breakdown:\n");
+    		printf("Savings: $%.2f\n", savings);
+    		printf("Checking: $%.2f\n", checking);
+    		printf("Investment: $%.2f\n", investment);
+
+	
+	} else {
+		//Edge case
+		printf("Invalid input"); 
+	}
 
 	return 0;
 }
