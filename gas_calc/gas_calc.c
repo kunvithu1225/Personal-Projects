821a52e7b299e177781427e989864ee3ded090d8
Aden <asvay@ucsc.edu>
2024-07-17 21:58:51 +0000

    first brainstorm of gas calc

---
+++ gas_calc/gas_calc.c
@@ -0,0 +1,57 @@
+// This is a gas calculator that takes away the daunting nature of calculating how much you have to pay your friend for gas < I see this as an informal uber that you use amongst friends >
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+	
+
+int main() {
+
+	// Inputs: 
+	// Distance of trip 
+	// Fuel effieceny of vehicle: potentially downloading car database API?
+	// Cost of fuel
+	// Number of people sharing cost 
+	// 
+	// - Tipping the driver ?
+	// - Electric Car ? 
+	//
+	//
+	//
+	// Calculations: 
+	// Total fuel from point A to point B 
+	// Total cost of fuel 
+	// Individual share of cost 
+	// Total plus the tip? 
+	//
+	//
+	//
+	// Output: 
+	// Display total fuel needed
+	// Display the total cost of the fuel 
+	// Display the amount each person owes 
+	// Display how much tip each person gave 
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
