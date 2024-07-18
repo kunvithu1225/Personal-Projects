9eb777fa38cbfb2026f1ac97ccf47908877c581d
Aden <asvay@ucsc.edu>
2024-07-18 19:28:17 +0000

    ran into security error, uploading without api key

--- chatbot_test/main.py
+++ chatbot_test/main.py
@@ -1,6 +1,6 @@
 import openai 
 
-openai.api_key = "sk-proj-J8xQmm68p5cyj28kMlArT3BlbkFJCxWtzJHKJbM72on2qwX3"
+openai.api_key = ""
 
 def chat_with_gpt(prompt):
     response = openai.ChatCompletion.create(
