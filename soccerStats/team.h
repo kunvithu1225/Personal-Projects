a958bbac9826892edfc83668060600535a951152
Aden <asvay@ucsc.edu>
2024-07-19 16:10:41 +0000

    made structure for team.h

--- soccerStats/team.h
+++ soccerStats/team.h
@@ -1,26 +1,34 @@
 // This is a file for the stats of a team. In this file we are using information from player.h 
-
+#ifdef TEAM_H
+#define TEAM_H
 
 
 #include <iostream>
 #include <vector>
 #include "player.h"
 
+class Team {
+public:
+    std::string teamName;
+    std::string coachName;
+    std::vector<Player> players; // Vector to store Player objects
+    int totalGoals;
+    int totalWins;
+    int totalLosses;
 
+    // Constructor
+    Team(const std::string& name, const std::string& coach);
 
+    // Function to add a player to the team
+    void addPlayer(const Player& player);
 
+    // Function to display team information
+    void displayTeamInfo();
 
+    // Function to update team statistics
+    void updateTeamStats(int goals, int wins, int losses);
+};
 
-
-
-
-
-
-
-
-
-
-
-
+#endif // TEAM_H
 
 
