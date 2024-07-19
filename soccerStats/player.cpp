63aaff02ed0e9739984266a4352bec60e1ddea82
Aden <asvay@ucsc.edu>
2024-07-19 16:28:01 +0000

    finished framework player.cpp

--- soccerStats/player.cpp
+++ soccerStats/player.cpp
@@ -2,14 +2,26 @@
 
 #include "player.h"
 
+// Constructor implementation
+Player::Player(const std::string& playerName, const std::string& playerPosition)
+    : name(playerName), position(playerPosition), goalsScored(0), assists(0), yellowCards(0), redCards(0) {}
 
+// Function to display player information
+void Player::displayInfo() {
+    std::cout << "Name: " << name << "\n"
+              << "Position: " << position << "\n"
+              << "Goals Scored: " << goalsScored << "\n"
+              << "Assists: " << assists << "\n"
+              << "Yellow Cards: " << yellowCards << "\n"
+              << "Red Cards: " << redCards << std::endl;
+}
 
-
-
-
-
-
-
-
+// Function to update player statistics
+void Player::updateStats(int goals, int assists, int yellow, int red) {
+    goalsScored += goals;
+    this->assists += assists;
+    yellowCards += yellow;
+    redCards += red;
+}
 
 
