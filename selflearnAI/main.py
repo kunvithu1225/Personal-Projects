3a125f5f415261f0ad31437628f6552855e467f1
Aden <asvay@ucsc.edu>
2024-07-25 21:41:37 +0000

    commenting out code for main.py

--- selflearnAI/main.py
+++ selflearnAI/main.py
@@ -2,23 +2,26 @@ import json
 from difflib import get_close_matches
 
 
-
+# Loading knowledge from JSON file
 def load_knowledge_base(file_path: str) -> dict:
     with open(file_path, 'r') as file:
         data: dict = json.load(file)
     return data 
 
 
+# Saving knowledge base to a JSON file 
 def save_knowledge_base(file_path: str, data: dict):
     with open(file_path, 'w') as file: 
         json.dump(data, file, indent=2)
 
 
+# Finding best match from the users question
 def find_best_match(user_question: str, questions: list[str]) -> str | None:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.7)
     return matches[0] if matches else None
 
 
+# Getting answer for a matched question
 def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
     for q in knowledge_base["questions"]:
         if q["question"] == question: 
@@ -26,6 +29,7 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
 
 
 
+# Main function
 def chatbot(): 
     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
 
