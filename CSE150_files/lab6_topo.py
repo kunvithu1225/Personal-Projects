d67a6c61ea3cad1626f8ecbeaa8375e0d51259cb
Aden <cambosoccer25@gmail.com>
2025-02-17 13:06:59 -0800

    working on assigning MAC addresses for hosts

--- CSE150_files/lab6_topo.py
+++ CSE150_files/lab6_topo.py
@@ -8,28 +8,39 @@ class MyTopology(Topo):
   def __init__(self):
     Topo.__init__(self)
    
-    # ADDING HOSTS 
+                    # ADDING HOSTS #
+
     # laptop1 = self.addHost('Laptop1', ip='200.20.2.8/24',defaultRoute="Laptop1-eth1")
-    facultyWS = self.addHost('facultyWS', ip='169.233.3.10/24', mac="00:00:00:00:01:02")
-    labWS = self.addHost('labWS', ip='169.233.4.2/24', mac="00:00:00:00:01:03")
+    
+    # University Data Center /24
+    examServer = self.addHost('examServer', ip='169.233.2.1/24', mac="00:00:00:00:10:02")
+    webServer = self.addHost('webServer', ip='169.233.2.2/24', mac="00:00:00:00:10:03")
+    dnsServer = self.addHost('dnsServer', ip='169.233.2.3/24', mac="00:00:00:00:10:04")
+
+    # IT Department LAN
     itWS = self.addHost('itWS', ip='169.233.1.10/24', mac="00:00:00:00:02:02")
     itBackup = self.addHost('itBackup', ip='169.233.1.30/24', mac="00:00:00:00:02:03")
-    
+    itPC = self.addHost('itPC', ip='169.233.1.20', mac=)
+
+    # faculty LAN /24
+    facultyWS = self.addHost('facultyWS', ip='169.233.3.10/24', mac="00:00:00:00:01:02")
+    printer = self.addHost('printer', ip='169.233.3.30/24', mac="00:00:00:00:10:12")
+    facultyPC = self.addHost('facultyPC', ip='169.233.3.30/24', mac=)
+
+    # student housing LAN /24
     studentPC1 = self.addHost('studentPC1', ip='169.233.4.1/24', mac="00:00:00:00:03:02")
     studentPC2 = self.addHost('studentPC2', ip='169.233.4.2/24', mac="00:00:00:00:03:03")
-    
+    labWS = self.addHost('labWS', ip='169.233.4.2/24', mac="00:00:00:00:01:03")
+
+    # Internet
+    # No subnet mask in the pdf? /24 or /32
     trustedPC1 = self.addHost('trustedPC1', ip='212.26.59.102', mac="00:00:00:00:04:02")
     trustedPC2 = self.addHost('trustedPC2', ip='10.100.198.6', mac="00:00:00:00:04:03")
     guest = self.addHost('guest', ip='10.100.198.10', mac="00:00:00:00:05:02")
 
-    examServer = self.addHost('examServer', ip='169.233.2.1/24', mac="00:00:00:00:10:02")
-    webServer = self.addHost('webServer', ip='169.233.2.2/24', mac="00:00:00:00:10:03")
-    dnsServer = self.addHost('dnsServer', ip='169.233.2.3/24', mac="00:00:00:00:10:04")
-    printer = self.addHost('printer', ip='169.233.3.30/24', mac="00:00:00:00:10:12")
-
-
-
-    # ADDING SWITCHES
+    
+                    # ADDING SWITCHES #
+    
     # switch1 = self.addSwitch('s1') 
     s1 = self.addSwitch('s1'); # -> Faculty LAN
     s2 = self.addSwitch('s2'); # -> Student Housing LAN
@@ -38,6 +49,8 @@ class MyTopology(Topo):
     s5 = self.addSwitch('s5'); # -> Core Switch
 
     # self.addLink(laptop1, switch1, port1=1, port2=2)
+
+
 
 if __name__ == '__main__':
   #This part of the script is run when the script is executed
