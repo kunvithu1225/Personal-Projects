076a2fd41254ea2ad77be2d0b45a93ee28b8d279
Aden <cambosoccer25@gmail.com>
2025-02-22 13:31:17 -0800

    fixed typo in flood, fixing other functions like forward_packet, get_out_port , etc.

--- CSE150_files/lab6_controller.py
+++ CSE150_files/lab6_controller.py
@@ -14,16 +14,26 @@ class Routing(object):
         connection.addListeners(self)
         self.mac_to_port = {}
 
-    def get_out_port(self, dst_mac):
-        """Determining output port based on MAC address."""
-        return self.mac_to_port.get(dst_mac, of.OFPP_FLOOD)  # Default to flooding
+    def get_out_port(self, dst_ip, switch_id):
+    #Determines the correct output port based on IP address and switch ID."""
+        routing_table = {
+            "169.233.1": 1,  # IT Department
+            "169.233.2": 2,  # Data Center
+            "169.233.3": 3,  # Faculty
+            "169.233.4": 4,  # Student Housing
+            "169.233.5": 5,  # Internet
+        }
+        dst_subnet = ".".join(dst_ip.split(".")[:3])
+        return routing_table.get(dst_subnet, of.OFPP_FLOOD)
 
     def forward_packet(self, event, out_port):
-        """Forward packet using a flow rule."""
-        msg = of.ofp_packet_out()
-        msg.data = event.ofp
+        """Forward packet and install flow rule."""
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(event.packet)
         msg.actions.append(of.ofp_action_output(port=out_port))
+        msg.data = event.ofp  # Include the original packet
         self.connection.send(msg)
+
 
     def do_routing(self, packet, packet_in, port_on_switch, switch_id):
         """Routing logic."""
@@ -42,9 +52,11 @@ class Routing(object):
 
         # Allowing ARP Traffic to Fix Packet Losses
         if packet.find('arp'):
-            log.info("Allowing ARP Traffic")
-            self.forward_packet(event, of.OFFP_FLOOD)
-            return 
+            log.info(f"Handling ARP packet from {packet.src} -> {packet.dst}")
+            self.mac_to_port[packet.src] = event.port  # Learn MAC address
+            self.forward_packet(event, of.OFPP_FLOOD)  
+            return
+
 
 
         # Extract IPv4 addresses
