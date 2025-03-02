d76200551d8cf00db768aa6b04c5ce92214c76e0
Aden <cambosoccer25@gmail.com>
2025-03-02 00:13:48 -0800

    importing TCPClient.py code from 2.7 Textbook

--- CSE150_files/asvay-Part1.py
+++ CSE150_files/asvay-Part1.py
@@ -1,1 +1,14 @@
 
+
+# Code from 2.7 TCPClient.py
+from socket import *
+
+serverName = ’servername’
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+sentence = raw_input(’Input lowercase sentence:’)
+clientSocket.send(sentence.encode())
+modifiedSentence = clientSocket.recv(1024)
+print(’From Server: ’, modifiedSentence.decode())
+clientSocket.close()
