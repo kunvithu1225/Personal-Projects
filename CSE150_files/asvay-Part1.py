5788b7b0416d91b589c3b20c06f22514426023a9
Aden <cambosoccer25@gmail.com>
2025-03-02 00:16:43 -0800

    adding TCPServer pseudocode

--- CSE150_files/asvay-Part1.py
+++ CSE150_files/asvay-Part1.py
@@ -12,3 +12,18 @@ clientSocket.send(sentence.encode())
 modifiedSentence = clientSocket.recv(1024)
 print(’From Server: ’, modifiedSentence.decode())
 clientSocket.close()
+
+# Code from 2.7 TCPServer.py
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((’’, serverPort))
+serverSocket.listen(1)
+print(’The server is ready to receive’)
+while True:
+ connectionSocket, addr = serverSocket.accept()
+ sentence = connectionSocket.recv(1024).decode()
+ capitalizedSentence = sentence.upper()
+ connectionSocket.send(capitalizedSentence.encode())
+ connectionSocket.close()
