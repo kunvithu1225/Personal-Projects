8395be9c61b21ed0203d765b8e8030f01e134653
Aden <cambosoccer25@gmail.com>
2025-03-03 13:56:38 -0800

    updated code removed ServerTCP code

--- CSE150_files/asvay-Part1.py
+++ CSE150_files/asvay-Part1.py
@@ -8,36 +8,13 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-# Send REGISTER message
+# Sending REGISTER message
 register_msg = "REGISTER client123"
 clientSocket.send(register_msg.encode())
 
-# Send BRIDGE message
+# Sending BRIDGE message
 bridge_msg = "BRIDGE client456"
 clientSocket.send(bridge_msg.encode())
 
 print("Messages Sent!")
 clientSocket.close()
-
-
-
-# TCPServer.py
-from socket import *
-
-serverPort = 12000
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
-
-print('The server is ready to receive')
-      
-while True:
-    connectionSocket, addr = serverSocket.accept()
-
-    while True:
-        sentence = connectionSocket.recv(1024).decode()
-        if not sentence: 
-            break
-        print(f"Received: {sentence}")
-    
-    connectionSocket.close()
